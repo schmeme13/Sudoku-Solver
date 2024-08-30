821b4fe25481ddbf1887c5474b664da04d6969be
Logan Schmillen <schmillen20@gmail.com>
2024-08-29 19:28:52 -0500

    created sudoku grid with text fields for each square

--- App.java
+++ App.java
@@ -14,6 +14,24 @@ public class App {
         frame.setSize(400, 400);
         frame.setLayout(new BorderLayout());
 
+        // Creating the sudoku grid
+        JPanel sudokuPanel = new JPanel();
+        sudokuPanel.setLayout(new GridLayout(SIZE, SIZE));
+
+        // Add text fields to the grid
+        for(int i = 0; i < SIZE; i++) {
+            for(int j = 0; j < SIZE; j++) {
+                grid[i][j] = new JTextField();
+                grid[i][j].setHorizontalAlignment(JTextField.CENTER);
+                sudokuPanel.add(grid[i][j]);
+            }
+        }
+
+        // Add components
+        frame.add(sudokuPanel, BorderLayout.CENTER);
+
+        // Making the frame visible
         frame.setVisible(true);
     }
+
 }
