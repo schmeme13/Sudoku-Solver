534eab5e30d53b68c847bc3aca52a2d6a5410982
Logan Schmillen <schmillen20@gmail.com>
2024-09-02 18:54:30 -0500

    created a way to copy board information to solve

--- App.java
+++ App.java
@@ -41,8 +41,33 @@ public class App {
 
     //Method to call upon the class with the backtracking algorithm
     private void solveSudoku() {
-        JOptionPane.showMessageDialog(null, "Solving Sudoku...");
-        new Solve();
+        int[][] board = new int[SIZE][SIZE];
+
+        // Populate the board array with the data from the JTextField grid
+        for (int row = 0; row < SIZE; row++) {
+            for (int col = 0; col < SIZE; col++) {
+                String text = grid[row][col].getText();
+                if (text.isEmpty()) {
+                    board[row][col] = 0; // Empty cells are treated as 0
+                } else {
+                    board[row][col] = Integer.parseInt(text);
+                }
+            }
+        }
+
+        // Create an instance of the Solve class and pass the board to it
+        Solve solver = new Solve();
+        if (solver.solve(board)) {
+            // If solved, update the grid with the solution
+            for (int row = 0; row < SIZE; row++) {
+                for (int col = 0; col < SIZE; col++) {
+                    grid[row][col].setText(String.valueOf(board[row][col]));
+                }
+            }
+        } else {
+            // Display an error message if no solution exists
+            JOptionPane.showMessageDialog(null, "No solution exists!");
+        }
     }
 
 }
